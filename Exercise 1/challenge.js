acd37d82a288dc4730da58d5373e8692afda221b
kiruthikajayagopal <kiruthika.jayagopal@maplebots.com>
2017-06-03 14:04:22 +0530

    my tasks

---
+++ Exercise 1/challenge.js
@@ -0,0 +1,49 @@
+
+// Hello name
+
+var name = "kirti"
+console.log("Hello" + name)
+
+// modulus operation
+
+var x = 1125 % 2
+console.log(x)
+
+// Boolean OR operation
+
+var a = true
+var b = false
+var c = true
+console.log(a||b||c)
+
+//Boolean AND operation
+
+var a = true, b=false ,c=true
+console.log(a && b && c );
+
+// comparison
+
+var x = 10 - 3 * 5/4 + 202 % 3
+var y = 10 - (3 * (5/4) + (202 % 3))
+console.log( x,y)
+
+// number to string
+
+var num=12
+var a = num.toString()
+console.log(a)
+
+// string to num
+
+var a = parseInt("113")
+console.log(a)
+
+// string to float
+var a = parseFloat("1.12356")
+console.log(a)
+
+// string into number
+
+var a = parseInt("Helo kiruthika")
+console.log(a)
+ 
