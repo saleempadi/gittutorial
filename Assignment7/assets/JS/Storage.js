b2fbbdedfad8888d76b07721f87e76930c410c90
sasikumar <sasi.kumar@maplebots.com>
2017-06-08 15:52:34 +0530

    modify

--- Assignment7/assets/JS/Storage.js
+++ Assignment7/assets/JS/Storage.js
@@ -1,33 +1,78 @@
-function SaveItem(pagetitle,pageauthor) {
+function SaveItem(pagetitle,pageauthor,contnt) {
 	// this.title= document.forms.Journal.title.value;
 	//this.author = document.forms.Journal.author.value;
-	this.title=pagetitle
-	this.author=pageauthor
+	//this.title=pagetitle
+	//this.author=pageauthor
+//	this.contnt=contnt
 
-	if (this.title == "" || this.author == "") {
+
+
+	if (this.title == "" || this.author == ""|| this.contnt == "") {
 		alert("Please Enter All Details....!")
 	}
 	else {
-		localStorage.setItem(this.title, this.author);
+
+
+var addItem = function () {
+   
+    var oldItems = JSON.parse(localStorage.getItem('journal13')) || [];
+    var newItem =[{title1: pagetitle, author1: pageauthor,content1:contnt},];
+    oldItems.push(newItem);
+    localStorage.setItem('journal13', JSON.stringify(oldItems));
+//localStorage.setItem("journal11", JSON.stringify(journals));
+
+	
+	/*  Old File 
+	var journals= [{entry: pagetitle, ttle: pageauthor,athor:contnt},];
+
+	localStorage.setItem("journal11", JSON.stringify(journals));
+		alert("Added Successfully....!")*/
 		alert("Added Successfully....!")
-		document.forms.Journal.author.value=""
-		document.forms.Journal.title.value=""
-		doShowAll();
-	}
+	};
+	addItem();
+	doShowAll();
+}
 }
 function Insert()
 {
-var Insertitem=new SaveItem(document.forms.Journal.title.value,document.forms.Journal.author.value)
+var Insertitem=new SaveItem(document.forms.Journal.title.value,document.forms.Journal.author.value,document.forms.Journal.contnt.value)
 }
 
 
 function RemoveItem() {
-	this.title = document.forms.Journal.title.value;
-	document.forms.Journal.title.value = localStorage.removeItem(this.title);
+	/*this.title = document.forms.Journal.title.value;
+	document.forms.Journal.title.value = localStorage.removeItem("journal13", title(this.title));
 	alert("Removed Successfully....!")
-	document.forms.Journal.title.value=""
-	doShowAll();
-}
+	doShowAll();*/
+	
+
+
+  
+  //  oldItems.pop(newItem);
+
+var retrievedScores = JSON.parse(localStorage.getItem("journal13"));
+
+
+		for (i = 0; i <= retrievedScores.length - 1; i++) {
+			this.key = localStorage.key(i);
+
+		var array =	retrievedScores[i][0].title1;
+		if (document.forms.Journal.title.value==array)
+		{
+			var index = i;
+			if (index > -1) {
+   			 array.splice(index, 1);
+			alert("Removed Successfully....!")
+		
+
+		}
+
+			//	document.writeln((i+1)+ " "+retrievedScores[i].entry + " "+ retrievedScores.ttle + " "+  retrievedScores.athor);
+		}
+
+
+
+}}
 /*
 function removes()
 {
@@ -49,17 +94,25 @@ function doShowAll() {
 	if (CheckBrowser()) {
 		this.key = "";
 		//var aut="";
-		this.list = "<tr><th>IDCode</th><th>Title</th><th>Author</th></tr>\n";
+		this.list = "<tr><th>IDCode</th><th>Title</th><th>Author</th><th>Content</th></tr>\n";
 		i = 0;
-		for (i = 0; i <= localStorage.length - 1; i++) {
+
+
+		var retrievedScores = JSON.parse(localStorage.getItem("journal13"));
+
+
+		for (i = 0; i <= retrievedScores.length - 1; i++) {
 			this.key = localStorage.key(i);
 
-			this.list += "<tr><td>" + "ID0" + (i + 1) + "</td>\n<td>"
-				+ this.key + "</td>\n<td>"
-				+ localStorage.getItem(this.key) + "</td></tr>\n";
+			this.list += "<tr><td>" + (i+1)+ "</td>\n<td>"+retrievedScores[i][0].title1 + "</td>\n<td>"
+				+ retrievedScores[i][0].author1 + "</td>\n<td>"
+				+  retrievedScores[i][0].content1+ "</td></tr>\n";
+
+			//	document.writeln((i+1)+ " "+retrievedScores[i].entry + " "+ retrievedScores.ttle + " "+  retrievedScores.athor);
 		}
-		if (this.list == "<tr><th>IDCode</th><th>Title</th><th>Author</th></tr>\n") {
-			this.list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
+		//document.write(localStorage.getItem("journal13"));
+		if (this.list == "<tr><th>IDCode</th><th>Title</th><th>Author</th><th>Content</th></tr>\n") {
+			this.list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td>\n<td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
 		}
 		document.getElementById('list').innerHTML = this.list;
 	} else {
