e6e9c2d8813de8a2d8c41edd3aa0c1e4f8f75b1e
gowtham somasundaram <gowtham.somasundaram@maplebots.comgit config --global user.name  gowtham>
2017-06-14 17:59:47 +0530

    tasks

---
+++ exercise 9/boot.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {              
+    $('i.glyphicon-thumbs-up').click(function(){    
+        var $this = $(this),
+        c = $this.data('count');    
+        if (!c) c = 0;
+        c++;
+        $this.data('count',c);
+        $('#'+this.id+'-bs3').html(c);
+    });      
+    $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+        event.preventDefault();
+     
+    });                                        
+}); 
